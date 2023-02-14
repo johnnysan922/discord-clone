e3dfcbd5bfbe143b98a7f869cff5c8b97e7bc8eb
Johnny Sanchez <johnnyss1999@gmail.com>
2023-02-14 12:47:55 -0500

    Created 'appSlice' file

---
+++ src/features/appSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    channelId: null,
+    channelName: null,
+};
+
+export const appSlice = createSlice({
+  name: 'app',
+  initialState,
+  // The `reducers` field lets us define reducers and generate associated actions
+  reducers: {
+    setChannelId: (state, action) => {
+      state.app += action.payload;
+    },
+  },
+});
+
+export const { setChannelId } = appSlice.actions;
+
+//SELECTORS
+//THESE CAN BE OBTAINED FROM ANYWHERE IN THE APPLICATION
+export const selectChannelId = (state) => state.app.channelId;
+export const selectChannelName = (state) => state.app.channelName;
+
+export default appSlice.reducer;
